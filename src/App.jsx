a2cbbc1ab29f5b8f2249420c2bfc4e9c5a3db3ba
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-10-23 19:00:33 +0200

    render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import General from './components/General';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="app">
+      <div className="infoContainer">
+        <General />
+        <Education />
+        <Experience />
+      </div>
+
+      <div className="cv-placeholder"></div>
+    </div>
   );
 }
 
