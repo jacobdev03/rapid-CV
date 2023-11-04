29c54a1e17dfa4ed61068691818b3bdb815b254f
Jacob <73615055+jacobdev03@users.noreply.github.com>
2023-11-04 23:34:23 +0100

    simplify component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,122 +2,78 @@ import { useState } from 'react';
 import FormGroup from './FormGroup';
 import './styles/Section.css';
 
-const Education = () => {
-  const [form, setForm] = useState({
-    degree: '',
-    school: '',
-    city: '',
-    country: '',
-    startDate: '',
-    endDate: '',
-  });
+const Education = ({ form, handleChange, handleSubmit, educationData }) => {
+  const [showForm, setShowForm] = useState(false);
 
-  const [degree, setDegree] = useState('');
-  const [school, setSchool] = useState('');
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setForm({
-      degree: degree,
-      school: school,
-      city: city,
-      country: country,
-      startDate: startDate,
-      endDate: endDate,
-    });
-    setDegree('');
-    setSchool('');
-    setCity('');
-    setCountry('');
-    setStartDate('');
-    setEndDate('');
+  const handleAdd = () => {
+    setShowForm(!showForm);
   };
-
-  const handleChange = (e) => {
-    console.log(e.target.id);
-    switch (e.target.id) {
-      case 'degree':
-        setDegree(e.target.value);
-        break;
-      case 'school':
-        setSchool(e.target.value);
-        break;
-      case 'city':
-        setCity(e.target.value);
-        break;
-      case 'country':
-        setCountry(e.target.value);
-        break;
-      case 'startDate':
-        setStartDate(e.target.value);
-        break;
-      case 'endDate':
-        setEndDate(e.target.value);
-        break;
-    }
-  };
-
-  console.log(form);
 
   return (
     <div className="section">
       <h1>Education information</h1>
-      <form onSubmit={handleSubmit}>
-        <FormGroup
-          value={degree}
-          name={'Degree'}
-          labelText={'degree'}
-          type={'text'}
-          placeholder={'Degree'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={school}
-          name={'School'}
-          labelText={'school'}
-          type={'text'}
-          placeholder={'School'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={city}
-          name={'City'}
-          labelText={'city'}
-          type={'text'}
-          placeholder={'City'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={country}
-          name={'Country'}
-          labelText={'country'}
-          type={'text'}
-          placeholder={'Country'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={startDate}
-          name={'Start date'}
-          labelText={'startDate'}
-          type={'date'}
-          placeholder={'Start date'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={endDate}
-          name={'End date'}
-          labelText={'endDate'}
-          type={'date'}
-          placeholder={'End date'}
-          handleChange={handleChange}
-        />
+      <div className="education-data">
+        <button className="show-form-btn" onClick={handleAdd}>
+          Education +
+        </button>
+        {showForm && (
+          <form onSubmit={handleSubmit} id="education">
+            <FormGroup
+              value={educationData.degree}
+              name={'Degree'}
+              labelText={'degree'}
+              type={'text'}
+              placeholder={'Degree'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={educationData.school}
+              name={'School'}
+              labelText={'school'}
+              type={'text'}
+              placeholder={'School'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={educationData.city}
+              name={'City'}
+              labelText={'city'}
+              type={'text'}
+              placeholder={'City'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={educationData.country}
+              name={'Country'}
+              labelText={'country'}
+              type={'text'}
+              placeholder={'Country'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={educationData.startDate}
+              name={'Start date'}
+              labelText={'startDate'}
+              type={'date'}
+              placeholder={'Start date'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={educationData.endDate}
+              name={'End date'}
+              labelText={'endDate'}
+              type={'date'}
+              placeholder={'End date'}
+              handleChange={handleChange}
+            />
 
-        <button>Add</button>
-      </form>
+            <button>Add</button>
+          </form>
+        )}
+        {form.map((edu) => (
+          <h3 key={edu.id}>{edu.degree}</h3>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,107 +2,78 @@ import { useState } from 'react';
 import FormGroup from './FormGroup';
 import './styles/Section.css';
 
-const Experience = () => {
-  const [form, setForm] = useState({
-    jobTitle: '',
-    company: '',
-    startDate: '',
-    endDate: '',
-    description: '',
-  });
+const Experience = ({ form, handleChange, handleSubmit, experienceData }) => {
+  const [showForm, setShowForm] = useState(false);
 
-  const [jobTitle, setJobTitle] = useState('');
-  const [company, setCompany] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [description, setDescription] = useState('');
-
-  const handleSubmit = (e) => {
+  const handleAdd = (e) => {
     e.preventDefault();
-    setForm({
-      jobTitle: jobTitle,
-      company: company,
-      startDate: startDate,
-      endDate: endDate,
-      description: description,
-    });
-    setJobTitle('');
-    setCompany('');
-    setStartDate('');
-    setEndDate('');
-    setDescription('');
+
+    setShowForm(!showForm);
   };
 
-  const handleChange = (e) => {
-    console.log(e.target.id);
-    switch (e.target.id) {
-      case 'jobTitle':
-        setJobTitle(e.target.value);
-        break;
-      case 'company':
-        setCompany(e.target.value);
-        break;
-      case 'startDate':
-        setStartDate(e.target.value);
-        break;
-      case 'endDate':
-        setEndDate(e.target.value);
-        break;
-      case 'description':
-        setDescription(e.target.value);
-        break;
-    }
+  const handleSave = (e) => {
+    e.preventDefault();
+    handleSubmit(e);
+    setShowForm(!showForm);
   };
-
-  console.log(form);
 
   return (
     <div className="section">
       <h1>Experience information</h1>
-      <form onSubmit={handleSubmit}>
-        <FormGroup
-          value={jobTitle}
-          name={'Job Title'}
-          labelText={'jobTitle'}
-          type={'text'}
-          placeholder={'Job title'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={company}
-          name={'Company'}
-          labelText={'company'}
-          type={'text'}
-          placeholder={'Company'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={startDate}
-          name={'Start date'}
-          labelText={'startDate'}
-          type={'date'}
-          placeholder={'Start date'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={endDate}
-          name={'End date'}
-          labelText={'endDate'}
-          type={'date'}
-          placeholder={'End date'}
-          handleChange={handleChange}
-        />
-        <FormGroup
-          value={description}
-          name={'Description of job'}
-          labelText={'description'}
-          type={'textarea'}
-          placeholder={'Description'}
-          handleChange={handleChange}
-        />
+      <div className="experience-data">
+        <button className="show-form-btn" onClick={handleAdd}>
+          Experience +
+        </button>
+        {showForm && (
+          <form onSubmit={handleSave} id="experience">
+            <FormGroup
+              value={experienceData.jobTitle}
+              name={'Job Title'}
+              labelText={'jobTitle'}
+              type={'text'}
+              placeholder={'Job title'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={experienceData.company}
+              name={'Company'}
+              labelText={'company'}
+              type={'text'}
+              placeholder={'Company'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={experienceData.startDate}
+              name={'Start date'}
+              labelText={'startDate'}
+              type={'date'}
+              placeholder={'Start date'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={experienceData.endDate}
+              name={'End date'}
+              labelText={'endDate'}
+              type={'date'}
+              placeholder={'End date'}
+              handleChange={handleChange}
+            />
+            <FormGroup
+              value={experienceData.description}
+              name={'Description of job'}
+              labelText={'description'}
+              type={'textarea'}
+              placeholder={'Description'}
+              handleChange={handleChange}
+            />
 
-        <button>Add</button>
-      </form>
+            <button>Add</button>
+          </form>
+        )}
+        {form.map((exp) => (
+          <h3 key={exp.id}>{exp.jobTitle}</h3>
+        ))}
+      </div>
     </div>
   );
 };
